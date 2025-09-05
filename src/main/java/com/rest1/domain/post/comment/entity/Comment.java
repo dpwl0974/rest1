d1c2bada977f0e96e663624641839640413d2d34
dpwl0974 <dpwl0974@gmail.com>
2025-09-05 15:26:21 +0900

    feat: Post와 Comment 사이 순환 참조 고리 끊어서 post는 json 조회 시 나오지 않게

--- src/main/java/com/rest1/domain/post/comment/entity/Comment.java
+++ src/main/java/com/rest1/domain/post/comment/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.rest1.domain.post.comment.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.rest1.domain.post.post.entity.Post;
 import com.rest1.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
@@ -16,6 +17,8 @@ public class Comment extends BaseEntity {
 
     private String content;
     @ManyToOne
+    //@Getter(AccessLevel.PRIVATE) // getter가 있지만 private라 안됨 -> 즉, post 무시 -> 하지만 필요할 때 못 씀
+    @JsonIgnore // json 무시해라 -> getter.private 보다 나은 방법 -> 즉, 순환 참조 고리 끊기
     private Post post;
 
     public void update(String content) {
--- src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
@@ -5,19 +5,19 @@ import com.rest1.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController // json 형태인 @responsebody 생략 가능
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/posts") // 모든 메소드에 자동 매핑
 public class ApiV1PostController {
 
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
-    @ResponseBody
+    @GetMapping // 주소 생략 -> 간결
     @Transactional(readOnly = true)
     public List<Post> list() {
         return postService.findAll();
