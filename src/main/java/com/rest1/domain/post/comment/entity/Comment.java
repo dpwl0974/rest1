ad77583739638164ea0f218d8cb6683aba4ca810
dpwl0974 <dpwl0974@gmail.com>
2025-09-05 14:23:08 +0900

    init: 초기 세팅

---
+++ src/main/java/com/rest1/domain/post/comment/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.rest1.domain.post.comment.entity;
+
+import com.rest1.domain.post.post.entity.Post;
+import com.rest1.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor // 내가 사용 용도
+@NoArgsConstructor // JPA가 사용 용도
+@Getter
+public class Comment extends BaseEntity {
+
+    private String content;
+
+    @ManyToOne //Many -> 외래키 부여
+    private Post post;
+
+    public void update(String content) {
+        this.content = content;
+    }
+}
