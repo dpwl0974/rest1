d6cfcc740cc5789aba53f0a83959a9f3dd088b05
dpwl0974 <dpwl0974@gmail.com>
2025-09-08 14:09:27 +0900

    feat: 게시글 삭제 구현 - Object 타입 사용으로 모든 데이터 사용

--- src/main/java/com/rest1/global/rsData/RsData.java
+++ src/main/java/com/rest1/global/rsData/RsData.java
@@ -1,16 +1,18 @@
 package com.rest1.global.rsData;
 
 
-import com.rest1.domain.post.comment.dto.CommentDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class RsData {  // 조회를 제외한 모든 요청에 대한 응답의 필수요소 담은 클래스
+public class RsData{  // 조회를 제외한 모든 요청에 대한 응답의 필수요소 담은 클래스
 
     private String resultCode;
     private String msg;
-    private CommentDto data; // 부가 data - comment 정보
+
+    // 부가 data - comment 정보
+    // Object - PostDto, CommentDto 둘 다 사용
+    private Object data;
 
 }
