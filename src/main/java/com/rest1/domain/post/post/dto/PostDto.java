4e2c1d901af54ac7a523a7a4d9d6fc4822f4aa65
dpwl0974 <dpwl0974@gmail.com>
2025-09-08 10:47:56 +0900

    refactor: record 도입 - dto를 record로 변경하여 불변성 보장

--- src/main/java/com/rest1/domain/post/post/dto/PostDto.java
+++ src/main/java/com/rest1/domain/post/post/dto/PostDto.java
@@ -1,24 +1,25 @@
 package com.rest1.domain.post.post.dto;
 
 import com.rest1.domain.post.post.entity.Post;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-
-@Getter
-public class PostDto {
-    private Long id;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
-    private String subject; // 엔터티 건드릴 필요없이 dto에서 이름 변경
-    private String body;
-
-    public PostDto(Post post) {
-        this.id = post.getId();
-        this.createDate = post.getCreateDate();
-        this.modifyDate = post.getModifyDate();
-        this.subject = post.getTitle();
-        this.body = post.getContent();
+// lombok없이 생성자 자동 생성
+// record 도입 - private 등 자동 생성으로 인해 불변성 보장
+public record PostDto (
+        Long id,
+        LocalDateTime createDate,
+        LocalDateTime modifyDate,
+        String subject, // 엔터티 건드릴 필요없이 dto에서 이름 변경
+        String body
+){
+    public PostDto(Post post){
+        this(
+                post.getId(),
+                post.getCreateDate(),
+                post.getModifyDate(),
+                post.getTitle(),
+                post.getContent()
+        );
     }
 }
