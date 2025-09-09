1296dd48049e8e777962f6b22267ea2e4f9c7124
dpwl0974 <dpwl0974@gmail.com>
2025-09-09 15:03:03 +0900

    feat: 2부 시작 - Post, Comment TEST 생성

---
+++ src/test/java/com/rest1/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -0,0 +1,26 @@
+package com.rest1.domain.post.post.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+public class ApiV1PostControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void t1() {
+
+    }
+
+}
