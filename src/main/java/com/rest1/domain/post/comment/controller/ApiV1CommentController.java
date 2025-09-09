70c946769562600c6c06ba3e663c8ab17db8d03d
dpwl0974 <dpwl0974@gmail.com>
2025-09-09 14:22:13 +0900

    feat: 댓글 생성 구현 - PostMapping

--- src/main/java/com/rest1/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/rest1/domain/post/comment/controller/ApiV1CommentController.java
@@ -5,6 +5,9 @@ import com.rest1.domain.post.comment.entity.Comment;
 import com.rest1.domain.post.post.entity.Post;
 import com.rest1.domain.post.post.service.PostService;
 import com.rest1.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +35,8 @@ public class ApiV1CommentController {
     public CommentDto getItem(@PathVariable Long postId, @PathVariable Long commentId) {
         Post post = postService.findById(postId).get();
         Comment comment = post.findCommentById(commentId).get();
+
+
         return new CommentDto(comment);
     }
 
@@ -54,4 +59,36 @@ public class ApiV1CommentController {
                 "%d번 댓글이 삭제되었습니다.".formatted(commentId)
         );
     }
+
+    record CommentWriteReqBody(
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String content
+    ) {
+    }
+
+    record CommentWriteResBody(
+            CommentDto commentDto
+    ) {}
+
+    // 댓글 생성
+    @PostMapping("/{postId}/comments")
+    @Transactional
+    public RsData<CommentWriteResBody> createItem(
+            @PathVariable Long postId,
+            @RequestBody @Valid CommentWriteReqBody reqBody
+    ) {
+
+        Post post = postService.findById(postId).get();
+        Comment comment = postService.writeComment(post, reqBody.content);
+
+        return new RsData<>(
+                "201-1",
+                "%d번 게시물이 생성되었습니다.".formatted(post.getId()),
+                new CommentWriteResBody(
+                        new  CommentDto(comment)
+                )
+        );
+    }
+
 }
